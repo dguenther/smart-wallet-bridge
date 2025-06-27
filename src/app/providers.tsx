b60b2a3dcf7da7368200355de3c06d3b0acf51bf
Derek Guenther <derek.guenther@coinbase.com>
2025-06-27 09:39:40 -0400

    Remove rainbowkit

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,54 +2,14 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/style/theme";
-import "@rainbow-me/rainbowkit/styles.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { http, WagmiProvider, createConfig } from "wagmi";
-import {
-  connectorsForWallets,
-  RainbowKitProvider,
-  darkTheme,
-} from "@rainbow-me/rainbowkit";
-
-import {
-  metaMaskWallet,
-  walletConnectWallet,
-  rainbowWallet,
-  safeWallet,
-  coinbaseWallet,
-} from "@rainbow-me/rainbowkit/wallets";
 import { supportedChains } from "@/utils/supportedChains";
 
-const appName = "smart-wallet-bridge";
-const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!;
-
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: "Popular",
-      wallets: [
-        metaMaskWallet,
-        coinbaseWallet,
-        // Use WalletConnect with a custom storage prefix
-        // This is to prevent clashes with our walletkit in wallet/bridge.
-        ({ projectId }) =>
-          walletConnectWallet({
-            projectId,
-            options: {
-              customStoragePrefix: "rainbowkit-client-role-",
-            },
-          }),
-        rainbowWallet,
-        safeWallet,
-      ],
-    },
-  ],
-  { appName, projectId }
-);
 
 export const config = createConfig({
-  connectors: [...connectors],
+  connectors: [],
   chains: supportedChains,
   transports: supportedChains.reduce<Record<number, ReturnType<typeof http>>>(
     (transport, chain) => {
@@ -67,9 +27,7 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
     <ChakraProvider theme={theme}>
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider theme={darkTheme()} modalSize={"compact"}>
             {children}
-          </RainbowKitProvider>
         </QueryClientProvider>
       </WagmiProvider>
     </ChakraProvider>
